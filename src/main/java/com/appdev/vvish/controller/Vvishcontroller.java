65792202e9b1a8a3fd8933c27bbb22ae2e943f86
Abhishek <Abhishek.14.java@gmail.com>
2017-11-02 01:20:19 -0500

    added rest endpoint that takes selected media files

--- src/main/java/com/appdev/vvish/controller/Vvishcontroller.java
+++ src/main/java/com/appdev/vvish/controller/Vvishcontroller.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,10 @@ public class Vvishcontroller {
 	public ResponseEntity<String> getUserVideos(@RequestParam String groupId) {
 		return vVService.generateVideo(groupId);
 	}
-
+	@PostMapping(value = "/generateVideo/{userId}/{groupId}", 
+			produces = { MediaType.APPLICATION_JSON_VALUE },
+			consumes = {MediaType.APPLICATION_JSON_VALUE} )
+	public  String[] getUserSelectedVideos(@RequestBody String[] mediaFiles) {
+		return mediaFiles;	
+	}
 }
